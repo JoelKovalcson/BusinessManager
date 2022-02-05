1af161f347f4ed5039a8b7994fff21e079073c7d
Joel Kovalcson <joelkovalcson@gmail.com>
2022-02-04 20:00:31 -0500

    Added budget route for a department

--- routes/department.js
+++ routes/department.js
@@ -21,4 +21,9 @@ async function getEmployees(connection, department) {
 	return rows;
 }
 
-module.exports = {getDepartments, addDepartment, updateDepartment, deleteDepartment, getEmployees};
+async function getBudget(connection, department) {
+	let [rows] = await connection.execute('SELECT department.name, SUM(role.salary) AS budget FROM department LEFT JOIN role ON role.department_id = department.id LEFT JOIN employee ON employee.role_id = role.id WHERE department.id = ?', [department.id]);
+	return rows;
+}
+
+module.exports = {getDepartments, addDepartment, updateDepartment, deleteDepartment, getEmployees, getBudget};
