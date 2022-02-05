0aa67e91cffa10f73be5c4a7f4faca410d654258
Joel Kovalcson <joelkovalcson@gmail.com>
2022-02-04 20:41:40 -0500

    Updated project name

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS employee_manager;
-CREATE DATABASE employee_manager;
-USE employee_manager;
+DROP DATABASE IF EXISTS business_manager;
+CREATE DATABASE business_manager;
+USE business_manager;
